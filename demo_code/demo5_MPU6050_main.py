3c6baeb281f88a051e90aec3113a2b402ca7cfe5
yoyojacky <yoyojacky2009@gmail.com>
2022-09-21 19:01:16 +0800

    update demo code
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- demo_code/demo5_MPU6050_main.py
+++ demo_code/demo5_MPU6050_main.py
@@ -2,15 +2,16 @@ from machine import Pin, I2C
 from time import sleep
 import mpu6050
 import math
-from ssd1306 import SSD1306_I2C
-import framebuf
 
-WIDTH = 128
-HEIGHT = 64
+# from ssd1306 import SSD1306_I2C
+# import framebuf
+
+# WIDTH = 128
+# HEIGHT = 64
 
 buffer = bytearray(b'\x00\x00\x00\x00\x01\xf0\x0f\x00\x06\x860\xb0\x04\x01@\x10\x04\x01\x000\x02\x10\x89 \x02\x05\xd2 \x01\x03\xe0@\x01\x87\xe0\x80\x10~?\x00\x00\x88\x11\x80\x01\x18\x08@\x02><@\x02a\xc3@C\xc1\x81\xe0\x05\x80\x90\x91\x08\x80\x80\x98\x08\x81\xc0\x88\t\x83\xe1\x98)\xe4\x1f\x98\x07\xf8\x0e0\x068\x0c \x02\x18\x080\x02\x88\x08 \x03\x0e0A\x01\x8f\xf8\x80\x00|\x1f\x00\x008\x0c\x00\x00\x0c0\x02\x00\x03\xe0\x00\x00\x00\x80\x00\x01\x00\x00\x04')
 
-fb = framebuf.FrameBuffer(buffer, 32, 32, framebuf.MONO_HLSB)
+# fb = framebuf.FrameBuffer(buffer, 32, 32, framebuf.MONO_HLSB)
 
 
 # define variable to store imu data.
@@ -26,7 +27,7 @@ imu_data = {}
 # ------------------------
 i2c = I2C(1, sda=Pin(26), scl=Pin(25), freq=400000)
 
-oled_i2c = I2C(0, sda=Pin(19), scl=Pin(18), freq=400000)
+# oled_i2c = I2C(0, sda=Pin(19), scl=Pin(18), freq=400000)
 
 
 devices = i2c.scan()
@@ -41,7 +42,7 @@ for device in devices:
 
 # create imu object 
 imu = mpu6050.accel(i2c)
-oled = SSD1306_I2C(WIDTH, HEIGHT, oled_i2c)
+# oled = SSD1306_I2C(WIDTH, HEIGHT, oled_i2c)
 
 # initializing 3 axis value. 
 accx = 0 
@@ -77,20 +78,21 @@ while True:
     print('roll: {:.2f}, pitch: {:.2f}, yaw:{:.2f}'.format(roll_angle,
         pitch_angle,
         yaw_angle))
-    oled.fill(0)
-    x = int(64+64*pitch_angle)
-    y = int(32+32*roll_angle)
-    if x >= 128:
-        x = 128
-    elif x <=0:
-        x = 0
-
-    if y >= 64:
-        y = 64 
-    elif y <=0:
-        y = 0
-    
-    oled.blit(fb, x, y)
-    oled.show()
-    sleep(0.01)
-    oled.fill(0)
+    sleep(0.2)
+   # oled.fill(0)
+#    x = int(64+64*pitch_angle)
+#    y = int(32+32*roll_angle)
+#    if x >= 128:
+#        x = 128
+#    elif x <=0:
+#        x = 0
+#
+#    if y >= 64:
+#        y = 64 
+#    elif y <=0:
+#        y = 0
+#    
+#    oled.blit(fb, x, y)
+#    oled.show()
+#    sleep(0.01)
+#    oled.fill(0)
